acb603cae5a9d34c290248c3984cee577a526934
Matthew Powell <ST10504996@myemeris.edu.za>
2026-03-03 09:27:03 +0200

    change wording

--- test/calculatorgithub/CalculatorTest.java
+++ test/calculatorgithub/CalculatorTest.java
@@ -7,13 +7,13 @@ public class CalculatorTest {
     @Test
     public void addsTwoNumbers (){
         Calculator calculator = new Calculator();
-        assertEquals("1+1 should equal 2", 2, calculator.add(1,1));
+        assertEquals("1+1 = 2", 2, calculator.add(1,1));
     }
     
     @Test
     public void subtractsTwoNumbers (){
         Calculator calculator = new Calculator();
-        assertEquals("2-1 should equal 1", 1, calculator.subtract(2,1));
+        assertEquals("2-1 = 1", 1, calculator.subtract(2,1));
     }
 }
 
